8aa4ca52fd9c1e00dba8cfc6c11f95bc6d518f6b
castrosuellenx <castrosuellenx@gmail.com>
2024-06-21 18:28:23 -0300

    add List component with empty version

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,33 @@
 import styles from './List.module.css';
 
 export function List() {
-  return <p>List</p>;
+  const tasksLength = 0;
+  const tasksCompleted = 0;
+
+  return (
+    <div>
+      <div className={styles.listHeader}>
+        <aside>
+          <p>
+            Tarefas criadas <span>{tasksLength}</span>
+          </p>
+        </aside>
+
+        <aside>
+          <p>
+            Concluídas <span>{tasksCompleted}</span>
+          </p>
+        </aside>
+      </div>
+
+      <div className={styles.emptyListContent}>
+        <img src="./clipboard.png" alt="" />
+        <p>
+          <strong>Você ainda não tem tarefas cadastradas</strong>
+          <br />
+          <span>Crie tarefas e organize seus itens a fazer</span>
+        </p>
+      </div>
+    </div>
+  );
 }
