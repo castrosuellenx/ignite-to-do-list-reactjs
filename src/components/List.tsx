f0ea1f8ca75bc046c9173b04d908b1a0fe803780
castrosuellenx <castrosuellenx@gmail.com>
2024-06-21 19:10:52 -0300

    add initial Task at List component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,4 @@
+import {CheckCircle, Circle, Trash} from 'phosphor-react';
 import {EmptyList} from './EmptyList';
 import styles from './List.module.css';
 import {ListHeader} from './ListHeader';
@@ -21,7 +22,43 @@ export function List({tasks}: Props) {
     <div>
       <ListHeader tasksLength={tasksLength} tasksCompleted={tasksCompleted} />
 
-      {isEmptyList ? <EmptyList /> : <div className={styles.listContent}></div>}
+      {isEmptyList ? (
+        <EmptyList />
+      ) : (
+        <div className={styles.content}>
+          {tasks.map((task) => (
+            <div className={styles.taskContainer} key={task.id}>
+              {task.isCompleted ? (
+                <button
+                  className={styles.taskCheckbox}
+                  type="button"
+                  title="Marcar tarefa"
+                >
+                  <CheckCircle size={20} weight="fill" />
+                </button>
+              ) : (
+                <button
+                  className={styles.taskCheckboxCompleted}
+                  type="button"
+                  title="Desmarcar tarefa"
+                >
+                  <Circle size={20} />
+                </button>
+              )}
+
+              <p className={styles.taskDescription}>{task.description}</p>
+
+              <button
+                className={styles.taskTrashButton}
+                type="button"
+                title="Deletar tarefa"
+              >
+                <Trash size={16} weight="bold" color="var(--gray-300)" />
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
