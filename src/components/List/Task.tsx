9999072b6cac836a15bfc95630d839c2368bdd81
castrosuellenx <castrosuellenx@gmail.com>
2024-06-22 14:59:58 -0300

    add toggle and delete functions to Task component props

--- src/components/List/Task.tsx
+++ src/components/List/Task.tsx
@@ -10,9 +10,11 @@ export type TaskType = {
 
 type Props = {
   task: TaskType;
+  toggleCheckTask: (taskId: string) => void;
+  deleteTask: (taskId: string) => void;
 };
 
-export function Task({task}: Props) {
+export function Task({task, toggleCheckTask, deleteTask}: Props) {
   const checkboxClassName = `${styles.checkbox} ${
     task.isCompleted ? styles.checkboxChecked : styles.checkboxUnchecked
   }`;
@@ -21,6 +23,14 @@ export function Task({task}: Props) {
     task.isCompleted ? styles.descriptionChecked : ''
   }`;
 
+  function handleToggleCheckTask() {
+    toggleCheckTask(task.id);
+  }
+
+  function handleDeleteTask() {
+    deleteTask(task.id);
+  }
+
   return (
     <li className={styles.container} role="listitem">
       <button
@@ -28,6 +38,7 @@ export function Task({task}: Props) {
         type="button"
         title={task.isCompleted ? 'Uncheck task' : 'Check task'}
         aria-label={task.isCompleted ? 'Uncheck task' : 'Check task'}
+        onClick={handleToggleCheckTask}
       >
         {task.isCompleted ? <Check size={12} weight="bold" /> : null}
       </button>
@@ -39,6 +50,7 @@ export function Task({task}: Props) {
         type="button"
         title="Delete task"
         aria-label="Delete task"
+        onClick={handleDeleteTask}
       >
         <Trash size={16} weight="bold" />
       </button>
