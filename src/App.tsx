f1abdd639058707796b8e8796603f11049dc646b
castrosuellenx <castrosuellenx@gmail.com>
2024-06-22 15:00:23 -0300

    update App removing mocked tasks and adding real functionally

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import {useState} from 'react';
 import {PlusCircle} from 'phosphor-react';
+import {v4 as uuidv4} from 'uuid';
 
 import styles from './App.module.css';
 import {Header} from './components/Header';
@@ -7,53 +9,68 @@ import {ListHeader} from './components/List/ListHeader';
 import {EmptyList} from './components/List/EmptyList';
 import {Task, type TaskType} from './components/List/Task';
 
-const tasks: TaskType[] = [
-  {
-    id: '1',
-    description: 'Lorem ipsum dolor sit amet.',
-    isCompleted: false,
-  },
-  {
-    id: '2',
-    description:
-      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isCompleted: false,
-  },
-  {
-    id: '3',
-    description:
-      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isCompleted: false,
-  },
-  {
-    id: '4',
-    description:
-      'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isCompleted: true,
-  },
-  {
-    id: '5',
-    description:
-      'Lorem ipsum dolor sit amet. Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer. Lorem ipsum dolor sit amet. Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isCompleted: true,
-  },
-];
-
+// TODO: Move all logic to a custom hook
 // TODO: Responsiveness
 // TODO: i18n
 
 function App() {
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+  const [newTaskName, setNewTaskName] = useState('');
+
   const tasksLength = tasks.length;
   const tasksCompleted = tasks.filter((task) => task.isCompleted).length;
   const isEmptyList = tasksLength === 0;
+
+  function handleCreateNewTask(event: React.FormEvent) {
+    event.preventDefault();
+
+    const newTask: TaskType = {
+      id: uuidv4(),
+      description: newTaskName,
+      isCompleted: false,
+    };
+
+    setTasks((prevState) => [...prevState, newTask]);
+    setNewTaskName('');
+  }
+
+  function handleChangeNewTaskName(event: React.ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('');
+    setNewTaskName(event.target.value);
+  }
+
+  function handleNewTaskNameInvalid(
+    event: React.InvalidEvent<HTMLInputElement>
+  ) {
+    event.target.setCustomValidity("Task name can't be empty");
+  }
+
+  function onToggleCheckTask(taskId: string) {
+    setTasks((prevState) =>
+      prevState.map((task) =>
+        task.id === taskId ? {...task, isCompleted: !task.isCompleted} : task
+      )
+    );
+  }
+
+  function onDeleteTask(taskId: string) {
+    setTasks((prevState) => prevState.filter((task) => task.id !== taskId));
+  }
 
   return (
     <div className={styles.container}>
       <Header />
 
       <main className={styles.content}>
-        <form className={styles.formWrapper}>
-          <Input placeholder="Add a new task" aria-label="Task name" />
+        <form className={styles.formWrapper} onSubmit={handleCreateNewTask}>
+          <Input
+            placeholder="Add a new task"
+            aria-label="Task name"
+            value={newTaskName}
+            required
+            onChange={handleChangeNewTaskName}
+            onInvalid={handleNewTaskNameInvalid}
+          />
 
           <button
             className={styles.createButton}
@@ -75,7 +92,12 @@ function App() {
           ) : (
             <ul className={styles.taskListWrapper}>
               {tasks.map((task) => (
-                <Task key={task.id} task={task} />
+                <Task
+                  key={task.id}
+                  task={task}
+                  toggleCheckTask={onToggleCheckTask}
+                  deleteTask={onDeleteTask}
+                />
               ))}
             </ul>
           )}
